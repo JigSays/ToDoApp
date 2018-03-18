6cc2a01adbfc27f3165c6fcd3ce446e15f6cc961
Jordan Gary <JigSays@gmail.com>
2018-03-18 11:37:37 -0500

    step2 completed

---
+++ client/src/components/MenuPanel/Icon/Icon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Icon.css";
+import { Grid, Row, Col } from 'react-bootstrap';
+
+const Icon = props =>(
+    <Row>
+        <Col xs={12} className="icon">
+        X
+    </Col>
+    </Row>
+);
+
+export default Icon; 
--- client/src/components/MenuPanel/MenuPanel.js
+++ client/src/components/MenuPanel/MenuPanel.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./MenuPanel.css";
 import { Grid, Row, Col } from 'react-bootstrap';
+import Icon from "./Icon";
+
+const iconArr = [0,1,2,3,4];
 
 const MenuPanel = props => (
     <Col xs={1} className="menu-panel">
+    {iconArr.map(e=> <Icon />)}
     </Col>
 );
 
