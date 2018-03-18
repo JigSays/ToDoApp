82d74b469a6ae3607870e06e3b1af83e1c99bbc4
Cj Jordan <cj.jordan.tech@gmail.com>
2018-03-17 23:23:22 -0500

    adds component folders and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import TodoContainer from './TodoContainer';
 
 const App = () => (
   <div>
-
+    <TodoContainer />
   </div>
 );
 
---
+++ client/src/TodoContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Counter extends Component {
+    state = {
+    };
+  
+    render() {
+      return (
+        <div className="container-fluid">
+        </div>
+      );
+    }
+  }
+  
+  export default Counter;
